67fe49f4cf52d868453e7b55668e1e13644343c5
Magnebor <nsumanth107@gmail.com>
2020-05-25 19:58:38 +0530

    Add files via upload

---
+++ Open_CV_9.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+img = cv.imread('sample.jpeg',0)
+thresh = cv.adaptiveThreshold(img,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 20)
+cv.imshow('thresh', thresh)
+cv.imshow('image',img)
+cv.waitKey(0)
+cv.destroyAllWindows()
